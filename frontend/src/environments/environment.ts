f5add046afe01bda78fbea4e32d7cf4ac45ebfd0
Alexandre <alxandre.rbrt@gmail.com>
2024-06-18 22:09:19 +0200

    fix: cors

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,11 @@
+// export const environment = {
+//     production: false,
+//     backendProduct: 'http://localhost:3000/api/catalogue',
+//     backendUser: 'http://localhost:3000/api/user'
+// };
+
 export const environment = {
     production: false,
-    backendProduct: 'http://localhost:3000/api/catalogue',
-    backendUser: 'http://localhost:3000/api/user'
+    backendProduct: 'https://projet-web-kkyq.onrender.com/api/catalogue', // Remplacez par l'URL de votre backend sur Render
+    backendUser: 'https://projet-web-kkyq.onrender.com/api/user' // Remplacez par l'URL de votre backend sur Render
 };
