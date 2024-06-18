63901de0656f05769f0665349cacbca81b66ea2f
Alexandre <alxandre.rbrt@gmail.com>
2024-06-18 22:00:23 +0200

    fix: cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,19 @@ const users = require('./users.json');
 
 const SECRET_KEY = 'wav_scrt_key';
 
-app.use(cors({ origin: 'http://localhost:4200' }));
+const allowedOrigins = ['http://localhost:4200', 'https://project-frontend-9pbr.onrender.com/'];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  };
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
