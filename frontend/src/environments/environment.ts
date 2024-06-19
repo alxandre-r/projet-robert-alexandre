86dde6c0b6d7e29baf3558f56efb46b587ea7b7c
Alexandre <alxandre.rbrt@gmail.com>
2024-06-19 21:45:41 +0200

    feat: changed env variables to online backend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,9 +1,4 @@
 export const environment = {
-    backendProduct: 'http://localhost:3000/api/catalogue',
-    backendUser: 'http://localhost:3000/api/user'
+    backendProduct: 'https://projet-web-kkyq.onrender.com/api/catalogue', // Remplacez par l'URL de votre backend sur Render
+    backendUser: 'https://projet-web-kkyq.onrender.com/api/user' // Remplacez par l'URL de votre backend sur Render
 };
-
-// export const environment = {
-//     backendProduct: 'https://projet-web-kkyq.onrender.com/api/catalogue', // Remplacez par l'URL de votre backend sur Render
-//     backendUser: 'https://projet-web-kkyq.onrender.com/api/user' // Remplacez par l'URL de votre backend sur Render
-// };
